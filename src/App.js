2373164021963f41ff577869dc53323b8bce7bea
Goel Nathan <goelnathan@gmail.com>
2021-07-31 18:14:58 +0300

    from second computer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; //!! we use destructturing here !! So down there line 4 we would have written React.Component instead of Component
 import Navbar from './component/layout/Navbar';
 import Users from './component/users/Users';
+import Search from './component/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -12,7 +13,7 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data, loading: false });
     console.log(this.users);
   }
@@ -23,6 +24,7 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/component/users/Users.js
+++ src/component/users/Users.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import propTypes from 'prop-types'
 
-class Users extends Component {
-  // state = { //this is no longer used since we pass our state from the main component (App) as props in this component
-  //   users: [
-  //     {
-  //       id: '1',
-  //       login: 'mojombo',
-  //       avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-  //       html_url: 'https://github.com/mojombo',
-  //     },
-  //     {
-  //       id: '2',
-  //       login: 'defunkt',
-  //       avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-  //       html_url: 'https://github.com/defunkt',
-  //     },
-  //     {
-  //       id: '3',
-  //       login: 'pjhyett',
-  //       avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-  //       html_url: 'https://github.com/pjhyett',
-  //     },
-  //   ],
-  // };
-  render() {
+// class Users extends Component { //was a class based component, now is a functional component since we do not use states in it
+//   // state = { //this is no longer used since we pass our state from the main component (App) as props in this component
+//   //   users: [
+//   //     {
+//   //       id: '1',
+//   //       login: 'mojombo',
+//   //       avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+//   //       html_url: 'https://github.com/mojombo',
+//   //     },
+//   //     {
+//   //       id: '2',
+//   //       login: 'defunkt',
+//   //       avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+//   //       html_url: 'https://github.com/defunkt',
+//   //     },
+//   //     {
+//   //       id: '3',
+//   //       login: 'pjhyett',
+//   //       avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
+//   //       html_url: 'https://github.com/pjhyett',
+//   //     },
+//   //   ],
+//   // };
+//   render() {
+//     return (
+//       <div style={userStyle /* ref=style as a variable*/}>
+//         {this.props.users.map(
+//           (
+//             user //it was "this.state.users..." when the state was comming from this component
+//           ) => (
+//             <UserItem key={user.id} user={user} /> //here user is a prop that is passed to the userItem
+//           )
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+const Users = ({users, loading}) => {
+    if(loading) {
+      return <Spinner />
+    }
     return (
       <div style={userStyle /* ref=style as a variable*/}>
-        {this.props.users.map(
+        {users.map(
           (
             user //it was "this.state.users..." when the state was comming from this component
           ) => (
@@ -36,7 +56,11 @@ class Users extends Component {
         )}
       </div>
     );
-  }
+}
+
+Users.propTypes = {
+  users: propTypes.array.isRequired,
+  loading: propTypes.bool.isRequired
 }
 
 const userStyle = {
